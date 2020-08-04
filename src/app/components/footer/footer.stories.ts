7ea2cd4c3e519f5083f9295b23061503b47592ef
Awesome Fortune <fmaseko@jhb.dvt.co.za>
2020-08-04 19:52:45 +0200

    Implement footer component.

--- src/app/components/footer/footer.stories.ts
+++ src/app/components/footer/footer.stories.ts
@@ -1,12 +1,25 @@
 import { FooterComponent } from './footer.component';
-import { centered } from '@storybook/addon-centered/angular';
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { moduleMetadata } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
 
 export default {
   title: 'Footer',
-  component: FooterComponent,
-  decorators: [centered],
+  decorators: [
+    withKnobs,
+    moduleMetadata({
+      declarations: [FooterComponent],
+      imports: [CommonModule],
+    }),
+  ],
 };
 
 export const Default = () => ({
   component: FooterComponent,
+  props: {
+    companyName: text('Company name', 'Test'),
+  },
+  template: `
+  <app-footer [companyName]="companyName"></app-footer>
+  `,
 });
