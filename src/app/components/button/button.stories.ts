bdd787b937935bedd2c9fd28539bbbc28f176fc8
Awesome Fortune <fmaseko@jhb.dvt.co.za>
2020-08-05 07:22:36 +0200

    Add action button.

--- src/app/components/button/button.stories.ts
+++ src/app/components/button/button.stories.ts
@@ -1,12 +1,24 @@
 import { ButtonComponent } from './button.component';
 import { centered } from '@storybook/addon-centered/angular';
+import { text, withKnobs } from '@storybook/addon-knobs';
+import { moduleMetadata } from '@storybook/angular';
 
 export default {
   title: 'Button',
   component: ButtonComponent,
-  decorators: [centered],
+  decorators: [
+    centered,
+    withKnobs,
+    moduleMetadata({
+      declarations: [ButtonComponent],
+    }),
+  ],
 };
 
 export const Default = () => ({
   component: ButtonComponent,
+  props: {
+    text: text('Text', 'Test'),
+  },
+  template: `<app-button>{{ text }}</app-button>`,
 });
