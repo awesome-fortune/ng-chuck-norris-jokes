2fcb84d77d8857810570687ce87dc95635886670
Awesome Fortune <fmaseko@jhb.dvt.co.za>
2020-08-05 07:24:51 +0200

    Scaffold app layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { JokeContainerComponent } from './components/joke-container/joke-container.component';
 import { ButtonComponent } from './components/button/button.component';
+import { NavItemComponent } from './components/nav-item/nav-item.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, NavBarComponent, ToolbarComponent, JokeContainerComponent, ButtonComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, FooterComponent, NavBarComponent, ToolbarComponent, JokeContainerComponent, ButtonComponent, NavItemComponent, SearchInputComponent],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
